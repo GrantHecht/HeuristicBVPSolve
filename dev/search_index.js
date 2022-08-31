4e6d979c9ab2fb599ae56ede857c65a176df5a1b
Documenter.jl <documenter@juliadocs.github.io>
2022-08-31 17:49:39 +0000

    build based on b66b359

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = HeuristicBVPSolve","category":"page"},{"location":"#HeuristicBVPSolve","page":"Home","title":"HeuristicBVPSolve","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for HeuristicBVPSolve.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [HeuristicBVPSolve]","category":"page"}]
+}
